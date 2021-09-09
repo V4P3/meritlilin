341340232daf3121d0dc4f97ce711d7110f2c203
V4P3 <veveraerts@gmail.com>
2021-09-09 08:24:19 +0200

    protocol

--- checksum.py
+++ checksum.py
@@ -4,19 +4,30 @@ def addCheckSum(barray):
     barray.append(sum(barray[1:]) % 256)
     return barray
 
-  
-#EXAMPLES
+
+#EXAMPLES MLP1
+tiltU = bytearray.fromhex('DC 01 40 48 11 00')
+tiltD = bytearray.fromhex('DC 01 40 44 11 00')
+panL = bytearray.fromhex('DC 01 40 42 11 00')
+panR = bytearray.fromhex('DC 01 40 41 11 00')
+stop = bytearray.fromhex('DC 01 40 00 00 FF')
+apan = bytearray.fromhex('DC 01 40 06 00 00')
+
+#EXAMPLES MLP2
 tiltU = bytearray.fromhex('E0 01 00 48 11 00')
 tiltD = bytearray.fromhex('E0 01 00 44 11 00')
 panL = bytearray.fromhex('E0 01 00 42 11 00')
 panR = bytearray.fromhex('E0 01 00 41 11 00')
 stop = bytearray.fromhex('E0 01 00 00 00 FF')
 
+
+
 tiltUStr = addCheckSum(tiltU)
 tiltDStr = addCheckSum(tiltD)
 panLStr = addCheckSum(panL)
 panRStr = addCheckSum(panR)
 stopStr = addCheckSum(stop)
+apanStr = addCheckSum(apan)
 
 print('Tilt UP:',tiltUStr.hex())
 print('Tilt DOWN:',tiltDStr.hex())
--- test_prog2.py
+++ test_prog2.py
@@ -2,15 +2,23 @@ import serial
 import serial.rs485
 #import struct
 #import us
- 
+
+def addCheckSum(barray):
+    barray.append(sum(barray[1:]) % 256)
+    return barray
+
+
 ser = serial.Serial('com5', 9600)
 ser.rs485_mode = serial.rs485.RS485Settings(rts_level_for_tx=True, rts_level_for_rx=False, loopback=False, delay_before_tx=None, delay_before_rx=None)
 print(ser.isOpen())
-#thestring = bytearray.fromhex('DC 01 00 44 11 00 56')
-thestring = bytearray.fromhex("DC014006000047")
- 
+
+panL = bytearray.fromhex('DC 01 40 42 11 00')
+stop = bytearray.fromhex('DC 01 40 00 00 FF')
+apan = bytearray.fromhex('DC 01 40 06 00 00')
+thestring = addCheckSum(apan)
+
 print(thestring)
- 
+
 ser.write(thestring)
 s = ser.read(1)
 print(s)
